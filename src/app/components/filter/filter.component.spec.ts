08b4c5a00ab68089e4a766f23f970dd1bbb02a3e
tkoen93 <timokoen93@gmail.com>
2020-07-30 06:41:31 +0200

    Add new test to filter component

--- src/app/components/filter/filter.component.spec.ts
+++ src/app/components/filter/filter.component.spec.ts
@@ -1,102 +1,156 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { FilterComponent } from './filter.component';
-import { HttpService } from 'src/app/services/http.service';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { HttpService } from 'src/app/services/http.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LoaderService } from 'src/app/services/loader.service';
+import { of } from 'rxjs';
 import { VacancyTableComponent } from '../vacancy-table/vacancy-table.component';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { LoaderService } from 'src/app/services/loader.service';
+import { By } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('FilterComponent', () => {
-
-  let httpTestingController: HttpTestingController;
-
   let component: FilterComponent;
   let fixture: ComponentFixture<FilterComponent>;
 
+  const mockSkills = {
+    _embedded: {
+      skills: [
+        {
+            name: "Angular"
+        },
+        {
+            name: "Java"
+        }
+      ]
+    }
+  };
+
+  const mockVacancies = {
+      vacancies: [
+        {
+          id: "1",
+          vacancyURL: "url",
+          title: "title 1",
+          broker: "Yacht",
+          vacancyNumber: "1",
+          hours: "40",
+          location: "Amsterdam, Nederland",
+          salary: "1234",
+          postingDate: "21 april 2020",
+          about: "vacancy 1",
+          skills: []
+        },
+        {
+          id: "2",
+          vacancyURL: "url",
+          title: "title 2",
+          broker: "Huxley",
+          vacancyNumber: "2",
+          hours: "40",
+          location: "Amsterdam, Nederland",
+          salary: "2341",
+          postingDate: "21 april 2020",
+          about: "vacancy 2",
+          skills: []
+        },
+        {
+          id: "3",
+          vacancyURL: "url",
+          title: "title 3",
+          broker: "Jobbird",
+          vacancyNumber: "3",
+          hours: "40",
+          location: "Amsterdam, Nederland",
+          salary: "2143",
+          postingDate: "21 april 2020",
+          about: "vacancy 3",
+          skills: []
+        }
+      ]
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule,
-        MatAutocompleteModule
-      ],
-      providers: [
-        FormBuilder,
-        HttpService,
-        LoaderService,
-        { provide: MatDialog, useValue: {}},
-        { provide: MAT_DIALOG_DATA, useValue: []}
-      ],
-      declarations: [
-        FilterComponent,
-        VacancyTableComponent
-      ],
-      schemas: [NO_ERRORS_SCHEMA]
+        imports: [
+          HttpClientTestingModule,
+          MatAutocompleteModule
+        ],
+        providers: [
+          FormBuilder,
+          { provide: HttpService, useValue: jasmine.createSpyObj('HttpService', ['findAllSkills', 'getByQuery']) },
+          LoaderService,
+          { provide: MatDialog, useValue: {}},
+          { provide: MAT_DIALOG_DATA, useValue: []}
+        ],
+        declarations: [ 
+          FilterComponent,
+          VacancyTableComponent
+        ],
+        schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
+  }));
 
-    httpTestingController = TestBed.inject(HttpTestingController);
+  let httpServiceMock: jasmine.SpyObj<HttpService>;
+
+  beforeEach(() => {
+    httpServiceMock = TestBed.get(HttpService);
+
     fixture = TestBed.createComponent(FilterComponent);
     component = fixture.componentInstance;
-  }));
+    fixture.detectChanges();
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should get all vacancies on initialization', () => {
-    // This test should be added in filter.component.ts as we request the vacancies in that class?
-      // Create mock for vacancies http request
-      const vacanciesUrl = 'http://localhost:8080/searchrequest';
-      const mockVacancies = {
-          queries: [null, null, null],
-          vacancies: [
-            {
-                url: '1',
-                title: 'vacancy one',
-                broker: 'broker',
-                postingDate: 'date',
-                location: 'location',
-                vacancyUrl: 'url'
-            },
-            {
-                url: '2',
-                title: 'vacancy two',
-                broker: 'broker',
-                postingDate: 'date',
-                location: 'location',
-                vacancyUrl: 'url'
-            },
-            {
-                url: '3',
-                title: 'vacancy three',
-                broker: 'broker',
-                postingDate: 'date',
-                location: 'location',
-                vacancyUrl: 'url'
-            }
-          ]
-      };
+  it('should fill skills variable upon init', fakeAsync(() => {
+    // Arrange
+    let mockService = jasmine.createSpyObj('HttpService', ['findAllSkills', 'getByQuery']);
+    let filterComp = new FilterComponent(new FormBuilder, mockService, new LoaderService);
 
-      // Expect search request to not be triggered yet
-      httpTestingController.expectNone(vacanciesUrl);
-      expect(component.vacancies.length).toEqual(0);
+    mockService.findAllSkills.and.returnValue(of(mockSkills));
+    mockService.getByQuery.and.returnValue(of(null));
 
-      // Initialize component
-      fixture.detectChanges();
+    // Act
+    filterComp.ngOnInit();
+    tick();
 
-      // Expect one search request that should be a POST
-      const req = httpTestingController.expectOne(vacanciesUrl);
-      expect(req.request.method).toEqual('POST');
+    // Assert
+    expect(mockService.findAllSkills).toHaveBeenCalledTimes(1);
+    expect(mockService.getByQuery).toHaveBeenCalledTimes(1);
+    expect(filterComp.skills.length).toBe(2);
+  }));
 
-      // Respond to search request with mock data
-      req.flush(mockVacancies);
+  it('should fill vacancies variable upon init', fakeAsync(() => {
+    // Arrange
+    let mockService = jasmine.createSpyObj('HttpService', ['findAllSkills', 'getByQuery']);
+    let filterComp = new FilterComponent(new FormBuilder, mockService, new LoaderService);
 
-      // Expect stored vacancies to now match the mock data
-      expect(component.vacancies.length).toEqual(3);
-      expect(component.vacancies[0].title).toEqual(mockVacancies.vacancies[0].title);
-  });
+    const noSkills = {
+      _embedded: {
+          skills: []
+      }
+    };;
+
+    mockService.findAllSkills.and.returnValue(of(noSkills));
+    mockService.getByQuery.and.returnValue(of(mockVacancies));
+
+    // Act
+    filterComp.ngOnInit();
+    tick();
+
+    // Assert
+    expect(mockService.findAllSkills).toHaveBeenCalledTimes(1);
+    expect(mockService.getByQuery).toHaveBeenCalledTimes(1);
+    expect(filterComp.vacancies.length).toBe(3);
+    for(let i: number = 0; i < filterComp.vacancies.length; i++) {
+      expect(filterComp.vacancies[i].title).toBe("title " + (i+1));
+    }
+  }));
 });
