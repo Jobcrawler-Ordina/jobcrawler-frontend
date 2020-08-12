9ed4a7c7fce5bfd70b7e170fada664c81e01a5b8
tkoen93 <timokoen93@gmail.com>
2020-08-12 14:21:56 +0200

    Minor fix for retrieving skills in vacancy-dialog

--- src/app/components/vacancy-dialog/vacancy-dialog.component.ts
+++ src/app/components/vacancy-dialog/vacancy-dialog.component.ts
@@ -41,12 +41,13 @@ export class VacancyDialogComponent implements AfterContentInit {
   getVacancyDetails(vacancyID: string): void {
     this.httpService.getByID(vacancyID).subscribe((vacancy: any) => {
       vacancy.id = this.vacancyID;
-      // this.httpService.getSkillsForVacancy(vacancy._links.skills.href).subscribe((data: any) => {
       this.httpService.getSkillsForVacancy('/vacancies/' + vacancyID + '/skills').subscribe((data: any) => {
-      vacancy.skills = [];
-        data._embedded.skills.forEach(el => {
-          vacancy.skills.push(el.name);
-        });
+        vacancy.skills = [];
+        if(data._embedded) {
+          data._embedded.skills.forEach(el => {
+           vacancy.skills.push(el);
+          });
+        }
         this.vacancy = vacancy;
       });
   }, err => {
