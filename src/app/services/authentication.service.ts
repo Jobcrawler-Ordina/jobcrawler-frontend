f53d2579f13da675afe29a541abaee2e5c7096fa
tkoen93 <timokoen93@gmail.com>
2020-08-20 15:54:12 +0200

    Add authentication service

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+import { User } from '../models/user.model';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(username: string, password: string) {
+        return this.http.post<any>(environment.api + '/auth/signin', {
+            username: username,
+            password: password
+        })
+        .pipe(map(user => {
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.currentUserSubject.next(user);
+            return user;
+        }));
+    }
+
+    logout() {
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+
+    // getRole() {
+    //     return this.currentUserSubject.
+    // }
+}
