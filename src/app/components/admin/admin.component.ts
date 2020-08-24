ad9b6d66daa1221afa9dbf402c78d6275f4be3da
tkoen93 <timokoen93@gmail.com>
2020-08-24 11:17:45 +0200

    Add loading spinner to admin panel

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { LoaderService } from 'src/app/services/loader.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
@@ -8,7 +10,10 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService) { }
+  isLoading: Subject<boolean> = this.loaderService.isLoading;
+
+  constructor(private authenticationService: AuthenticationService,
+              private loaderService: LoaderService) { }
 
   ngOnInit(): void {
   }
