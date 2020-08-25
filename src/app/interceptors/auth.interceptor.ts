4188adba84dbfd9b1009def07865c62b377237af
tkoen93 <timokoen93@gmail.com>
2020-08-25 11:50:15 +0200

    Refresh JWT token if admin is still active

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,9 +3,15 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
 import { environment } from 'src/environments/environment';
+import * as moment from 'moment';
+import { User } from '../models/user.model';
+import { first } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+    refreshing: boolean = false;
+
     constructor(private authenticationService: AuthenticationService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -19,7 +25,21 @@ export class AuthInterceptor implements HttpInterceptor {
                         Authorization: `Bearer ${currentUser.token}`
                     }
                 });
+
+                let expiresInSec: number = moment(currentUser.expiresAt).diff(moment(), 'seconds');
+                if (expiresInSec < 240 && this.refreshing == false) { // Refresh JWT as user is still active
+                    this.refreshing = true;
+                    this.authenticationService.refresh()
+                    .pipe(first())
+                    .subscribe(
+                      () => {
+                          this.refreshing = false;
+                      }
+                    );
+                }
             }
+
+
 
             return next.handle(req);
         }
