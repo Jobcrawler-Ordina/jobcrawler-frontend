02ea04af619872b7575ba3c1de7bfd9f5e9712f2
tkoen93 <timokoen93@gmail.com>
2020-10-21 19:12:04 +0200

    Login-dialog component test WIP

--- src/app/components/login-dialog/login-dialog.component.spec.ts
+++ src/app/components/login-dialog/login-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { LoginDialogComponent } from './login-dialog.component';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
@@ -10,12 +10,13 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Router } from '@angular/router';
 import { loginMockResponse } from 'src/app/tests/authenticationServiceMockResponses';
 import { allowRegistrationMock } from 'src/app/tests/adminServiceMockResponses';
-import { BehaviorSubject, of, Observable } from 'rxjs';
-import { User } from 'src/app/models/user.model';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('LoginDialogComponent', () => {
   let component: LoginDialogComponent;
   let fixture: ComponentFixture<LoginDialogComponent>;
+  let nativeComponent: HTMLElement;
   let authSpy;
   let routerSpy;
 
@@ -47,6 +48,7 @@ describe('LoginDialogComponent', () => {
 
     fixture = TestBed.createComponent(LoginDialogComponent);
     component = fixture.componentInstance;
+    nativeComponent = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
@@ -69,4 +71,46 @@ describe('LoginDialogComponent', () => {
 
     expect(routerSpy.navigate).toHaveBeenCalledTimes(0);
   });
+
+  it('should succesful login user and redirect to /admin', () => {
+    authSpy.login.and.returnValue(of(loginMockResponse));
+    component.loginForm.controls.username.setValue('Admin');
+    component.loginForm.controls.password.setValue('password');
+    fixture.detectChanges();
+    const button = nativeComponent.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(authSpy.login).toHaveBeenCalledWith('Admin', 'password');
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/admin']);
+  });
+
+  it('should not login user and not call the navigate method', () => {
+    authSpy.login.and.returnValue(of());
+    component.loginForm.controls.username.setValue('Admin');
+    component.loginForm.controls.password.setValue('password');
+    fixture.detectChanges();
+    const button = nativeComponent.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+
+    expect(authSpy.login).toHaveBeenCalledWith('Admin', 'password');
+    expect(routerSpy.navigate).toHaveBeenCalledTimes(0);
+  });
+
+  it('should show 3 additional input fields after clicking on register', async(() => {
+    const inputsBeforeClicking = nativeComponent.querySelectorAll('input');
+
+    component.allowRegistration = true;
+    fixture.detectChanges();
+    const signUpTab = fixture.debugElement.queryAll(By.css('.mat-tab-label'))[1];
+    signUpTab.triggerEventHandler('click', {});
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+        const inputsAfterClicking = nativeComponent.querySelectorAll('input');
+        expect(inputsBeforeClicking.length).toBe(2);
+        expect(inputsAfterClicking.length).toBe(5);
+    });
+  }));
 });
